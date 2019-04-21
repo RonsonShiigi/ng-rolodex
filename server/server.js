7a165274cd62fec7204cbfefd52ea4a2c489a9dc
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-04-20 16:17:36 -1000

    added and installing shit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port : ${PORT}`);
+});
